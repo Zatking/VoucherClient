3eca8e733d99cc36d5b2ef690b0ce888ebf018d8
Zatking <117702923+Zatking@users.noreply.github.com>
2024-07-25 00:41:51 +0700

    commit for deploy

--- server.cjs
+++ server.cjs
@@ -4,10 +4,10 @@ const path = require('path')
 const app = express()  
 const port = process.env.port||5000;
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get("*", (req, res) => { 
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname,"dist","index.html"));
 })
 
 app.listen(port,() => {
