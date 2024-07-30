aa7ae1cfbb96bf95be551e6637edd29567819309
Zatking <117702923+Zatking@users.noreply.github.com>
2024-07-30 13:54:40 +0700

    commit

--- src/page/CreateVoucherPartner.jsx
+++ src/page/CreateVoucherPartner.jsx
@@ -65,151 +65,211 @@ q          });
     }
     const service = "Partner";
 
-    return (
-        <>
-        <h1 className="text-2xl font-bold mb-4 text-center">Tạo Voucher  {query}</h1>
-        <div className="w-full mx-auto p-6 bg-white shadow-md rounded-md align-middle">
-          <form  className="grid grid-cols-2 gap-10">
-            <div className="flex">
-              <label htmlFor="VoucherID" className=" text-sm font-medium text-gray-700">Mã Voucher:</label>
-              <input
-                type="text"
-                id="VoucherID"
-                name="VoucherID"
-                value={voucherPartner.VoucherID}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-                disabled={true}
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherName" className="block text-sm font-medium text-gray-700">Tên Voucher:</label>
-              <input
-                type="text"
-                id="VoucherName"
-                name="VoucherName"
-                value={voucherPartner.VoucherName}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherType" className="block text-sm font-medium text-gray-700">Loại Voucher:</label>
-              <input
-                type="text"
-                id="VoucherType"
-                name="VoucherType"
-                value={voucherPartner.VoucherType}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherImage" className="block text-sm font-medium text-gray-700">Ảnh Voucher:</label>
-              <input
-                type="text"
-                id="VoucherImage"
-                name="VoucherImage"
-                value={voucherPartner.VoucherImage}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherDescription" className="block text-sm font-medium text-gray-700">Mô Tả Voucher:</label>
-              <input
-                type="text"
-                id="VoucherDescription"
-                name="VoucherDescription"
-                value={voucherPartner.VoucherDescription}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherStartDate" className="block text-sm font-medium text-gray-700">Ngày Bắt Đầu:</label>
-              <input
-                type="date"
-                id="VoucherStartDate"
-                name="VoucherStartDate"
-                value={voucherPartner.VoucherStartDate}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherEndDate" className="block text-sm font-medium text-gray-700">Ngày Kết Thúc:</label>
-              <input
-                type="date"
-                id="VoucherEndDate"
-                name="VoucherEndDate"
-                value={voucherPartner.VoucherEndDate}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherDiscount" className="block text-sm font-medium text-gray-700">Giảm Giá (%):</label>
-              <input
-                type="number"
-                id="VoucherDiscount"
-                name="VoucherDiscount"
-                value={voucherPartner.VoucherDiscount}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherMinValue" className="block text-sm font-medium text-gray-700">Giá Trị Tối Thiểu:</label>
-              <input
-                type="number"
-                id="VoucherMinValue"
-                name="VoucherMinValue"
-                value={voucherPartner.VoucherMinValue}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherMaxValue" className="block text-sm font-medium text-gray-700">Giá Trị Tối Đa:</label>
-              <input
-                type="number"
-                id="VoucherMaxValue"
-                name="VoucherMaxValue"
-                value={voucherPartner.VoucherMaxValue}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>
-            <div className="flex">
-              <label htmlFor="VoucherQuantity" className="block text-sm font-medium text-gray-700">Số Lượng Voucher:</label>
-              <input
-                type="number"
-                id="VoucherQuantity"
-                name="VoucherQuantity"
-                value={voucherPartner.VoucherQuantity}
-                onChange={handleChange}
-                className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                required
-              />
-            </div>      
-            <button type="submit"
-             className="w-full bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-             onClick={handleSubmit}>Tạo Voucher</button>
-          </form>
-        </div>
-      
-      </>
-);
+    
+      return (
+        <form className="bg-gradient-to-b from-green-400 p-6">
+          <h1 className="text-4xl font-bold mb-4 pt-6 text-center text-white border-t-8 border-white rounded-3xl">
+            Tạo Voucher cho hệ thống {query}
+          </h1>
+          <div className="grid grid-cols-12 rounded-xl bg-white place-items-end items-center gap-4 p-12 m-12 drop-shadow-xl">
+            <p className="col-span-2 font-bold text-left text-green-400 text-lg place-self-start">
+              Thông tin cơ bản
+            </p>
+            <div className="col-span-10"></div>
+            <label
+              htmlFor="VoucherID"
+              className=" text-sm font-medium text-gray-700 col-span-2"
+            >
+              Mã Voucher:
+            </label>
+            <input
+              type="text"
+              id="VoucherID"
+              name="VoucherID"
+              value={voucherPartner.VoucherID}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+                focus:outline-none focus:ring-green-400 focus:border-green-400 sm:text-sm"
+              required
+              disabled
+            />
+            <label
+              htmlFor="VoucherName"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Tên Voucher:
+            </label>
+            <input
+              type="text"
+              id="VoucherName"
+              name="VoucherName"
+              value={voucherPartner.VoucherName}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+                focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherType"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Loại Voucher:
+            </label>
+            <input
+              type="text"
+              id="VoucherType"
+              name="VoucherType"
+              value={voucherPartner.VoucherType}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherImage"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Ảnh Voucher:
+            </label>
+            <input
+              type="text"
+              id="VoucherImage"
+              name="VoucherImage"
+              value={voucherPartner.VoucherImage}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+            />
+            <label
+              htmlFor="VoucherDescription"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Mô Tả Voucher:
+            </label>
+            <input
+              type="text"
+              id="VoucherDescription"
+              name="VoucherDescription"
+              value={voucherPartner.VoucherDescription}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 border col-span-10 border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+          </div>
+          <div className="grid grid-cols-12 rounded-xl bg-white place-items-end items-center gap-4 p-12 m-12 shadow-xl">
+            <p className="col-span-2 font-bold text-left text-green-400 text-lg place-self-start">
+              Thiết lập giảm giá
+            </p>
+            <div className="col-span-10"></div>
+            <label
+              htmlFor="VoucherStartDate"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Ngày Bắt Đầu:
+            </label>
+            <input
+              type="date"
+              id="VoucherStartDate"
+              name="VoucherStartDate"
+              value={voucherPartner.VoucherStartDate}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+                  focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherEndDate"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Ngày Kết Thúc:
+            </label>
+            <input
+              type="date"
+              id="VoucherEndDate"
+              name="VoucherEndDate"
+              value={voucherPartner.VoucherEndDate}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherDiscount"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Giảm Giá (%):
+            </label>
+            <input
+              type="number"
+              id="VoucherDiscount"
+              name="VoucherDiscount"
+              value={voucherPartner.VoucherDiscount}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherMinValue"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Giá Trị Tối Thiểu:
+            </label>
+            <input
+              type="number"
+              id="VoucherMinValue"
+              name="VoucherMinValue"
+              value={voucherPartner.VoucherMinValue}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherMaxValue"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Giá Trị Tối Đa:
+            </label>
+            <input
+              type="number"
+              id="VoucherMaxValue"
+              name="VoucherMaxValue"
+              value={voucherPartner.VoucherMaxValue}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <label
+              htmlFor="VoucherQuantity"
+              className="block text-sm font-medium text-gray-700 col-span-2"
+            >
+              Số Lượng Voucher:
+            </label>
+            <input
+              type="number"
+              id="VoucherQuantity"
+              name="VoucherQuantity"
+              value={voucherPartner.VoucherQuantity}
+              onChange={handleChange}
+              className="mt-1 block w-full px-3 py-2 col-span-4 border border-gray-300 rounded-md shadow-sm
+              focus:outline-none focus:ring-green-500 focus:border-green-500 sm:text-sm"
+              required
+            />
+            <button
+              type="submit"
+              className="w-full bg-green-400 text-white font-bold py-4 mt-8 col-span-8 col-start-3 rounded-lg
+              hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-400"
+              onClick={handleSubmit}
+            >
+              Tạo Voucher
+            </button>
+          </div>
+        </form>
+      );
+    
 }
 
 export default VoucherForm;
